69909cc88b295c53b3acb97955c93479528870f1
david <dtrdic2@gmail.com>
2024-01-22 14:51:28 +0100

    revert plugin repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("io.codemagic.dtrdic17").version("8.0.0").apply(false)
+    id("com.android.application").version("8.0.0").apply(false)
     id("com.android.library").version("8.0.0").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
